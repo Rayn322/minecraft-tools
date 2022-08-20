a5de529149e8e9facc7418403c513c7d22a79428
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-08-20 13:05:56 -0700

    Remove comments and unused import

--- src/routes/server/[ip]/+page.svelte
+++ src/routes/server/[ip]/+page.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-	// Suggestion (check code before using, and possibly convert to data.X access later):
-	// import type { PageData } from './$types';
-	// export let data: PageData;
-	// $: ({ response } = data);
-
-	import type { JavaStatusResponse } from 'minecraft-server-util';
 	import Server from '$lib/components/Server.svelte';
 	import type { PageData } from './$types';
 
